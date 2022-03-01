a08ef1c1d709c264edb39e80d2681a548b8414e4
MrLYC <imyikong@gmail.com>
2022-03-02 00:28:08 +0800

    feat: generate operation name by config

--- bkapi-client-core/internal/mock/operation.go
+++ bkapi-client-core/internal/mock/operation.go
@@ -55,6 +55,20 @@ func (mr *MockOperationMockRecorder) Apply(opts ...interface{}) *gomock.Call {
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Apply", reflect.TypeOf((*MockOperation)(nil).Apply), opts...)
 }
 
+// Name mocks base method.
+func (m *MockOperation) Name() string {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "Name")
+	ret0, _ := ret[0].(string)
+	return ret0
+}
+
+// Name indicates an expected call of Name.
+func (mr *MockOperationMockRecorder) Name() *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Name", reflect.TypeOf((*MockOperation)(nil).Name))
+}
+
 // Request mocks base method.
 func (m *MockOperation) Request() (*http.Response, error) {
 	m.ctrl.T.Helper()
